6fc7ef142f366f2d5b523a1dbee6e1a79f93aeab
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2023-11-19 01:37:48 +0530

    feat: setup broker for mumbai/polygon

--- contracts/src/root/Broker.sol
+++ contracts/src/root/Broker.sol
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract Broker {}
+interface IStateSender {
+    function syncState(address receiver, bytes calldata data) external;
+}
+
+error ChainIdNotSupported(uint32 chainId);
+
+contract Broker {
+    IStateSender public stateSender;
+    address public stateReceiver;
+
+    function setStateSender(address _stateSender) external {
+        stateSender = IStateSender(_stateSender);
+    }
+
+    function setStateReceiver(address _stateReceiver) external {
+        stateReceiver = _stateReceiver;
+    }
+
+    function sendMessage(
+        uint32 chainId,
+        address receiver,
+        bytes calldata data
+    ) external {
+        if (chainId == 80001) {
+            stateSender.syncState(receiver, data);
+        } else revert ChainIdNotSupported(chainId);
+    }
+}
